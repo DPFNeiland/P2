8eda8824ae3560ce5dc712f3e676997b30614fd2
Rodrigo Neiland <rodrigo.naohan@gmail.com>
2024-05-16 08:08:21 -0400

    update160520240808

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import AppBanco from "./Atividades/AppBanco/AppBanco";
 import Pinterest from "./Atividades/Pinterest/Pinterest";
+import Atividade1 from "./Atividades/Atividade1/Atividade1";
+import Atividade3 from "./Atividades/Atividade3/Atividade3";
 
 export default function App() {
   return (
-    <AppBanco></AppBanco>
+    <Atividade3 />
   );
 }
